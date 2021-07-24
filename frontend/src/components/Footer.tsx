a73efd3cc4f2373738e9a5aaf4f3d14f7dcabe9a
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2021-07-24 22:35:35 +0200

    footer updated

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -6,12 +6,9 @@ interface FooterProps {
 
 const Footer: React.FC<FooterProps> = ({ description }) => {
   return (
-    <footer className='p-3 bg-danger text-white text-center position-relative'>
+    <footer className='bg-light text-center position-relative footer mt-auto py-3'>
       <div className='container'>
         <p className='lead'>{description}</p>
-        <a href='/#' className='position-absolute bottom-0 end-0 p-3'>
-          <i className='bi bi-arrow-up-square-fill text-light h1'></i>
-        </a>
       </div>
     </footer>
   )
