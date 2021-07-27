389ba04c432bea6cf67d3c06c0d8195295f9ca41
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2021-07-27 17:43:23 +0200

    missing types and some reworks and workarounds for accordion

--- frontend/src/components/AccordionComponent.tsx
+++ frontend/src/components/AccordionComponent.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React from 'react'
+// import React, { useState } from 'react'
 import Accordion from 'react-bootstrap/Accordion'
 // import { Accordion } from 'react-bootstrap'
 
@@ -10,32 +11,32 @@ interface AccordionProps {
   titleOne: string
   descOne: string
 }
-interface AccordionStateProps {
-  status: 'open' | 'collapsed'
-  statusText: 'show' | 'collapse'
-}
+// interface AccordionStateProps {
+//   status: 'open' | 'collapsed'
+//   statusText: 'show' | 'collapse'
+// }
 
 const AccordionComponent: React.FC<AccordionProps> = ({
   link,
   titleOne,
   descOne,
 }) => {
-  const [accordionOne, setAccordionOne] = useState<AccordionStateProps>({
-    status: 'open',
-    statusText: 'show',
-  })
+  // const [accordionOne, setAccordionOne] = useState<AccordionStateProps>({
+  //   status: 'open',
+  //   statusText: 'show',
+  // })
 
-  const accOneHandler = () => {
-    if (accordionOne.status === 'collapsed') {
-      setAccordionOne({ status: 'open', statusText: 'show' })
-      console.log(accordionOne.statusText)
-    } else if (accordionOne.status === 'open') {
-      setAccordionOne({ status: 'collapsed', statusText: 'collapse' })
-      console.log(accordionOne.statusText)
-    } else {
-      console.log(accordionOne.statusText)
-    }
-  }
+  // const accOneHandler = () => {
+  //   if (accordionOne.status === 'collapsed') {
+  //     setAccordionOne({ status: 'open', statusText: 'show' })
+  //     console.log(accordionOne.statusText)
+  //   } else if (accordionOne.status === 'open') {
+  //     setAccordionOne({ status: 'collapsed', statusText: 'collapse' })
+  //     console.log(accordionOne.statusText)
+  //   } else {
+  //     console.log(accordionOne.statusText)
+  //   }
+  // }
   // const accOneHandler = () => {
   //   if (statusOne === 'collapsed') {
   //     setStatusOne('open')
@@ -50,8 +51,7 @@ const AccordionComponent: React.FC<AccordionProps> = ({
     <section id='sectionAccord' className='p-5 w-100'>
       <div className='container'>
         <h2 className='text-center mb-4'>Most important questions</h2>
-
-        <Accordion defaultActiveKey='0' className='text-dark'>
+        <Accordion defaultActiveKey='0' className='text-dark bg-light'>
           <Card className='bg-light'>
             <Card.Header as='div' className='text-center bg-light'>
               <Accordion.Toggle
@@ -64,7 +64,7 @@ const AccordionComponent: React.FC<AccordionProps> = ({
               </Accordion.Toggle>
             </Card.Header>
             <Accordion.Collapse eventKey='0'>
-              <Card.Body>body 1</Card.Body>
+              <Card.Body className='bg-warning'>body 1</Card.Body>
             </Accordion.Collapse>
           </Card>
           <Card>
@@ -79,11 +79,63 @@ const AccordionComponent: React.FC<AccordionProps> = ({
               </Accordion.Toggle>
             </Card.Header>
             <Accordion.Collapse eventKey='1'>
-              <Card.Body>body 2</Card.Body>
+              <Card.Body className='bg-info'>body 2</Card.Body>
+            </Accordion.Collapse>
+          </Card>
+          <Card>
+            <Card.Header as='div' className='text-center bg-light'>
+              <Accordion.Toggle
+                className='bg-light'
+                as={Card.Header}
+                variant='link'
+                eventKey='2'
+              >
+                title 3
+              </Accordion.Toggle>
+            </Card.Header>
+            <Accordion.Collapse eventKey='2'>
+              <Card.Body className='bg-success'>body 2</Card.Body>
             </Accordion.Collapse>
           </Card>
         </Accordion>
       </div>
+      {/*  @ts-ignore */}
+      <Accordion defaultActiveKey='0'>
+        {/* @ts-ignore: */}
+        <Accordion.Item eventKey='0'>
+          {/*  @ts-ignore */}
+          <Accordion.Header>Accordion Item #1</Accordion.Header>
+          {/*  @ts-ignore */}
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+            {/*  @ts-ignore */}
+          </Accordion.Body>
+          {/*  @ts-ignore */}
+        </Accordion.Item>
+        {/*  @ts-ignore */}
+        <Accordion.Item eventKey='1'>
+          {/*  @ts-ignore */}
+          <Accordion.Header>Accordion Item #2</Accordion.Header>
+          {/*  @ts-ignore */}
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+            {/*  @ts-ignore */}
+          </Accordion.Body>
+          {/*  @ts-ignore */}
+        </Accordion.Item>
+      </Accordion>
     </section>
   )
 }
