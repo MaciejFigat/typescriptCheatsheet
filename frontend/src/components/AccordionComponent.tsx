e72287dd2d256a8f81cafc9cec9c2558a4b26440
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2021-07-29 18:08:31 +0200

    changes in home

--- frontend/src/components/AccordionComponent.tsx
+++ frontend/src/components/AccordionComponent.tsx
@@ -1,58 +1,37 @@
 import React from 'react'
-// import React, { useState } from 'react'
 import Accordion from 'react-bootstrap/Accordion'
-// import { Accordion } from 'react-bootstrap'
 
 import Card from 'react-bootstrap/Card'
 
-// import Button from 'react-bootstrap/Button'
 interface AccordionProps {
-  link: string
+  linkOne: string
   titleOne: string
   descOne: string
+  linkTwo?: string
+  titleTwo?: string
+  descTwo?: string
+  linkThree?: string
+  titleThree?: string
+  descThree?: string
 }
-// interface AccordionStateProps {
-//   status: 'open' | 'collapsed'
-//   statusText: 'show' | 'collapse'
-// }
 
 const AccordionComponent: React.FC<AccordionProps> = ({
-  link,
+  linkOne,
   titleOne,
   descOne,
+  linkTwo,
+  titleTwo,
+  descTwo,
+  linkThree,
+  titleThree,
+  descThree,
 }) => {
-  // const [accordionOne, setAccordionOne] = useState<AccordionStateProps>({
-  //   status: 'open',
-  //   statusText: 'show',
-  // })
-
-  // const accOneHandler = () => {
-  //   if (accordionOne.status === 'collapsed') {
-  //     setAccordionOne({ status: 'open', statusText: 'show' })
-  //     console.log(accordionOne.statusText)
-  //   } else if (accordionOne.status === 'open') {
-  //     setAccordionOne({ status: 'collapsed', statusText: 'collapse' })
-  //     console.log(accordionOne.statusText)
-  //   } else {
-  //     console.log(accordionOne.statusText)
-  //   }
-  // }
-  // const accOneHandler = () => {
-  //   if (statusOne === 'collapsed') {
-  //     setStatusOne('open')
-  //     console.log(statusOne)
-  //   } else {
-  //     setStatusOne('collapsed')
-  //     console.log(statusOne)
-  //   }
-  // }
-
   return (
     <section id='sectionAccord' className='p-5 w-100'>
       <div className='container'>
         <h2 className='text-center mb-4'>{titleOne}</h2>
         <Accordion defaultActiveKey='0' className='text-dark bg-light'>
-          <Card className='bg-light'>
+          <Card>
             <Card.Header as='div' className='text-center bg-light'>
               <Accordion.Toggle
                 className='bg-light'
@@ -60,43 +39,47 @@ const AccordionComponent: React.FC<AccordionProps> = ({
                 variant='link'
                 eventKey='0'
               >
-                {link}
+                {linkOne}
               </Accordion.Toggle>
             </Card.Header>
             <Accordion.Collapse eventKey='0'>
               <Card.Body className='bg-warning'>{descOne}</Card.Body>
             </Accordion.Collapse>
           </Card>
-          <Card>
-            <Card.Header as='div' className='text-center bg-light'>
-              <Accordion.Toggle
-                className='bg-light'
-                as={Card.Header}
-                variant='link'
-                eventKey='1'
-              >
-                title 2
-              </Accordion.Toggle>
-            </Card.Header>
-            <Accordion.Collapse eventKey='1'>
-              <Card.Body className='bg-info'>body 2</Card.Body>
-            </Accordion.Collapse>
-          </Card>
-          <Card>
-            <Card.Header as='div' className='text-center bg-light'>
-              <Accordion.Toggle
-                className='bg-light'
-                as={Card.Header}
-                variant='link'
-                eventKey='2'
-              >
-                title 3
-              </Accordion.Toggle>
-            </Card.Header>
-            <Accordion.Collapse eventKey='2'>
-              <Card.Body className='bg-success'>body 2</Card.Body>
-            </Accordion.Collapse>
-          </Card>
+          {titleTwo && (
+            <Card>
+              <Card.Header as='div' className='text-center bg-light'>
+                <Accordion.Toggle
+                  className='bg-light'
+                  as={Card.Header}
+                  variant='link'
+                  eventKey='1'
+                >
+                  {titleTwo}
+                </Accordion.Toggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey='1'>
+                <Card.Body className='bg-info'>{descTwo}</Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          )}
+          {titleThree && (
+            <Card>
+              <Card.Header as='div' className='text-center bg-light'>
+                <Accordion.Toggle
+                  className='bg-light'
+                  as={Card.Header}
+                  variant='link'
+                  eventKey='2'
+                >
+                  {titleThree}
+                </Accordion.Toggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey='2'>
+                <Card.Body className='bg-success'>{descThree}</Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          )}
         </Accordion>
       </div>
     </section>
--- frontend/src/screens/Home.tsx
+++ frontend/src/screens/Home.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import AccordionComponent from '../components/AccordionComponent'
+// import AccordionComponent from '../components/AccordionComponent'
+import { LinkContainer } from 'react-router-bootstrap'
 const Home: React.FC = () => {
   return (
     <>
       <section
         id='showcase'
-        className='bg-dark text-light p-5 p-lg-0 pt-lg-5 h-100 text-center text-sm-start'
+        className='bg-dark text-light p-2 p-lg-0 pt-lg-5 h-100 text-center text-sm-start'
       >
         <div className='container'>
           <div className='d-sm-flex align-items-center justify-content-between'>
@@ -15,7 +16,15 @@ const Home: React.FC = () => {
                 <span className='text-success'>my cheatsheet</span>
               </h1>
               <p className='lead my-4'>
-                npx create-react-app projectXXX --template typescript
+                By understanding JavaScript, TypeScript saves you time catching
+                errors and providing fixes before you run code. - so{' '}
+                <a
+                  href='https://www.typescriptlang.org/'
+                  className='text-danger'
+                >
+                  they{' '}
+                </a>
+                say
               </p>
             </div>
           </div>
@@ -23,24 +32,45 @@ const Home: React.FC = () => {
       </section>
       <section
         id='showcase'
-        className='bg-dark text-light p-5 p-lg-0 pt-lg-5 text-center text-sm-start'
+        className='bg-dark text-light p-3 p-lg-0 pt-lg-5 text-center text-sm-start'
       >
         <div className='container'>
-          <div className='d-sm-flex align-items-center justify-content-between'>
+          <div className='d-sm-flex align-items-center justify-content-between position-static'>
             <div>
-              <h1>
-                <span className='text-info'>Typescript</span> for React{' '}
-                <span className='text-success'>my cheatsheet</span>
-              </h1>
+              <h2>
+                <LinkContainer to={'/hooks'}>
+                  <span className='text-warning'>Here</span>
+                </LinkContainer>{' '}
+                you can find <span className='text-info'>TS resources</span>{' '}
+                that I use in React development.
+              </h2>
               <p className='lead my-4'>
-                npx create-react-app projectXXX --template typescript
+                TypeScript’s
+                <a href='https://2ality.com/2018/04/type-notation-typescript.html'>
+                  <span className='text-warning'> type notation</span>
+                </a>{' '}
+                - by 2ality
+              </p>
+              <p className='lead my-4'>
+                Beautiful
+                <a href='https://github.com/typescript-cheatsheets/react/blob/main/README.md#basic-cheatsheet-table-of-contents'>
+                  <span className='text-info'> repository</span>
+                </a>{' '}
+                - contains further links
+              </p>
+              <p className='lead my-4'>
+                Official
+                <a href='https://www.typescriptlang.org/play?q=469#example/conditional-types'>
+                  <span className='text-success'> playground</span>
+                </a>{' '}
+              </p>
+              <p className='lead my-4'>
+                Official
+                <a href='https://www.typescriptlang.org/'>
+                  <span className='text-danger'> documentation</span>
+                </a>{' '}
               </p>
             </div>
-            <AccordionComponent
-              titleOne='Title one'
-              descOne='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-              link='link to a new world.com'
-            />
           </div>
         </div>
       </section>
@@ -48,3 +78,11 @@ const Home: React.FC = () => {
   )
 }
 export default Home
+
+{
+  /* <AccordionComponent
+  titleOne='Links to resources'
+  descOne='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  link='link to a new world.com'
+/> */
+}
